2228296cda370c1e6a2b0b52c5ad1c81d4e22b0d
Mohammadali94-ui <200611711@student.georgianc.on.ca>
2026-01-16 13:45:33 -0500

    Complete Lab One index.php

--- 01/index.php
+++ 01/index.php
@@ -1,5 +1,19 @@
 <?php
+// Include required files
+require "car.php";
+require "connect.php";
 
-echo "<h1> Hello World! </h1>"
+// Create a Car object
+$myCar = new Car("Toyota", "Corolla", 2017);
 
+// Display output
+echo "<h2>Car Information</h2>";
+echo "<p>" . $myCar->getCarInfo() . "</p>";
+
+/*
+Reflection:
+The most challenging part of this lab was setting up the database connection
+using PDO. The easiest part was creating the Car class and displaying the
+object information in the browser.
+*/
 ?>
